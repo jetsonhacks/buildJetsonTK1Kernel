0eebd5f1930edce474d2686ae0ce58993751178f
Jetsonhacks <jim@jetsonhacks.com>
2017-08-25 21:08:12 -0700

    Update to L4T 21.5

--- scripts/installKernelSources.sh
+++ scripts/installKernelSources.sh
@@ -3,7 +3,7 @@ apt-add-repository universe
 apt-get update
 apt-get install libqt4-dev pkg-config -y
 cd /usr/src
-wget http://developer.download.nvidia.com/embedded/L4T/r21_Release_v4.0/source/kernel_src.tbz2
+wget http://developer.download.nvidia.com/embedded/L4T/r21_Release_v5.0/source/kernel_src.tbz2
 tar -xvf kernel_src.tbz2
 cd kernel
 zcat /proc/config.gz > .config
